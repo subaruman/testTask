acaea56d10be157240e8cb216afe0b0c88b13b0a
subaruman <Centik.cent73@gmail.com>
2019-10-19 23:21:54 +0400

    created auth and admin page

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //DashBoard
+    public function dashboard(){
+        return view('admin.dashboard');
+    }
+}
